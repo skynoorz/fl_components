83d67fb7419cf5d92a1a193f3ad9a7bbf3cc8ddc
skynoor <ronaldg@qtm.bo>
2023-01-28 21:07:10 -0400

    ListView Builder

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -7,16 +7,21 @@ class ListViewBuilderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return FadeInImage(
-            width: double.infinity,
-            height: 300,
-            fit: BoxFit.cover,
-            placeholder: const AssetImage('assets/jar-loading.gif'), 
-            image: NetworkImage('https://picsum.photos/500/300?image=${index + 1}'));
-        },
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return FadeInImage(
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+              placeholder: const AssetImage('assets/jar-loading.gif'), 
+              image: NetworkImage('https://picsum.photos/500/300?image=${index + 1}'));
+          },
+        ),
       ),
     );
   }
